f33375facf9ad2052e3a48f10d97121852033718
Jia Harisinghani <jia2.harisinghani@gmail.com>
2025-05-11 18:29:54 +0530

    React pages added

---
+++ React with CSS/src/Pages/ToggleButton.js
@@ -0,0 +1,10 @@
+function ToggleButton({ toggleForm }) {
+  return (
+    <div className="img__btn" style={{ color: 'rgb(0, 0, 0)' }} onClick={toggleForm}>
+      <span className="m--up">Sign Up</span>
+      <span className="m--in">Login</span>
+    </div>
+  );
+}
+
+export default ToggleButton;
